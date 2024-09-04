3f5ae3ec44d9ec18d5513e1f4ab33969bdb5260c
julianFaltoni <168470958+julianFaltoni@users.noreply.github.com>
2024-09-04 15:41:28 -0300

    haca Implemente una ruta y el action de creasion de nuevo usuario deforma vasica falta haser las inportasiones

---
+++ src/main/kotlin/utn/methodology/infrastructure/http/actions/CreateUserAction.kt
@@ -0,0 +1,17 @@
+package example.com.infrastructure.http.actions
+
+import example.com.application.commands.CreateUserCommand
+import example.com.application.commandhandlers.CreateUserHandler
+
+class CreateUserAction(
+    private val handler: CreateUserHandler
+) {
+
+    fun execute(body: CreateUserCommand) {
+
+        body.validate().let {
+            handler.handle(it)
+        }
+
+    }
+}
---
+++ src/main/kotlin/utn/methodology/infrastructure/http/router/UserRouter.kt
@@ -0,0 +1,81 @@
+package example.com.infrastructure.http.router
+
+import example.com.application.commands.CreateUserCommand
+//import example.com.application.commands.UpdateUserCommand
+import example.com.application.commandhandlers.CreateUserHandler
+//import example.com.application.commandhandlers.DeleteUserHandler
+//import example.com.application.commandhandlers.UpdateUserHandler
+//import example.com.application.commands.DeleteUserCommand
+//import example.com.application.queries.FindUserByIdQuery
+//import example.com.application.queryhandlers.FindUserByIdHandler
+import example.com.infrastructure.http.actions.CreateUserAction
+//import example.com.infrastructure.http.actions.DeleteUserAction
+//import example.com.infrastructure.http.actions.FindUserByIdAction
+//import example.com.infrastructure.http.actions.UpdateUserAction
+/*import example.com.infrastructure.persistence.MongoUserRepository
+import example.com.infrastructure.persistence.connectToMongoDB
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.* */
+
+fun Application.userRouter() {
+    val mongoDatabase = connectToMongoDB()
+
+    val userMongoUserRepository = MongoUserRepository(mongoDatabase)
+
+    val createUserAction = CreateUserAction(CreateUserHandler(userMongoUserRepository))
+    /* 
+    val updateUserAction = UpdateUserAction(UpdateUserHandler(userMongoUserRepository))
+    val findUserByIdAction = FindUserByIdAction(FindUserByIdHandler(userMongoUserRepository))
+    val deleteUserAction = DeleteUserAction(DeleteUserHandler(userMongoUserRepository))
+    */
+    routing {
+
+        post("/users") {
+            val body = call.receive<CreateUserCommand>()
+            createUserAction.execute(body);
+
+            call.respond(HttpStatusCode.Created, mapOf("message" to "ok"))
+        }
+        /*
+        put("/users/{id}") {
+            val body = call.receive<UpdateUserCommand>()
+
+            body.id = call.parameters["id"].toString() ?: throw IllegalArgumentException("No ID Found")
+
+            updateUserAction.execute(body);
+
+            call.respond(HttpStatusCode.OK, mapOf("message" to "updated"))
+        }
+
+        get("/users/{id}") {
+
+            val query = FindUserByIdQuery(call.parameters["id"].toString())
+
+            val result = findUserByIdAction.execute(query)
+
+            call.respond(HttpStatusCode.OK, result)
+
+        }
+
+        get("/users") {
+            val users = userMongoUserRepository.findAll();
+
+            call.respond(HttpStatusCode.OK, users.map { it.toPrimitives() })
+        }
+
+
+        delete("/users/{id}") {
+
+            val query = DeleteUserCommand(call.parameters["id"].toString())
+
+            deleteUserAction.execute(query)
+
+            call.respond(HttpStatusCode.NoContent)
+        }
+        */
+    }
+}
