126c3360d722fa08bea2d7a58e21b24ad584becf
Juan Ignacio Garetto <juangarettooooooo@gmail.com>
2024-09-06 21:37:45 -0300

    Aver si nos ponemos las pilas.

--- src/main/kotlin/utn/methodology/domain/entities/User.kt
+++ src/main/kotlin/utn/methodology/domain/entities/User.kt
@@ -1,16 +1,22 @@
-package utn.methodology.domain.entities
+package utn.methodology.domain.entities.User
 
 import java.util.UUID
 
 data class User(
 
     var uuid: UUID = UUID.randomUUID(),
-
     var nombre: String,
-
     var username: String,
-
     var email: String,
-
     var contraseñia: String
-)
+){
+    fun toPrimitives(): Map<String, Any> {
+        return mapOf(
+            "uuid" to uuid.toString(),  // Convertir UUID a String
+            "nombre" to nombre,
+            "username" to username,
+            "email" to email,
+            "contrasenia" to contraseñia
+        )
+    }
+}
